752d4c5321dd1b6022d3f41a5d4ca250c921ca08
Ashran Khan <ashran.khan@kyndryl.com>
2024-03-03 15:59:56 +0530

    newb22

--- src/test1.java
+++ src/test1.java
@@ -2,9 +2,7 @@ public class test1 {
 
    public void foo(){
        System.out.println("in test1");
-       System.out.println("2nd");
-       System.out.println("3rd");
-       System.out.println("4rth");
+
        System.out.println("2273");
        System.out.println("2273");
    }
--- src/test2.java
+++ src/test2.java
@@ -2,7 +2,7 @@ public class test2 {
 
     public void boo(){
         System.out.println("in test2");
-        System.out.println("9");
         System.out.println("172");
+        System.out.println("asjfd f");
     }
 }
